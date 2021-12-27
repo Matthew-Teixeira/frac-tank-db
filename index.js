4dac7a955c19fa2e5a73956f3c23f72c11000052
ApolloSolo <matt1teixeira@gmail.com>
2021-12-27 18:19:51 -0500

    Now able to edit volume entries. Added a delete route in the edit page to delete entire entry

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/volumes/:zones/view', async (req, res) => {
 
 //Go to volume add page
 app.get('/volumes/new', (req, res) => {
-    res.render('tanks/new')
+    res.render('tanks/new') 
 })
 
 //Post new data
@@ -50,6 +50,25 @@ app.post('/volumes', async (req, res) => {
     const newVolume = new Volume(req.body)
     await newVolume.save(); //Saved to db
     res.redirect(`/volumes/${newVolume.zone}/view`);
+})
+
+//Edit an entry
+app.get('/volumes/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const foundVolume = await Volume.findById(id);
+    res.render('tanks/edit', { foundVolume });
+})
+
+app.put('/volumes/:id', async (req, res) => {
+    const { id } = req.params;
+    const volume = await Volume.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect('/volumes');
+})
+
+app.delete('/volumes/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedVolume = await Volume.findByIdAndDelete(id);
+    res.redirect('/volumes');
 })
 
 app.listen(3000, (req, res) => {

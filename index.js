f5a2371cc678854b77e039863492d659039f6e29
ApolloSolo <matt1teixeira@gmail.com>
2022-01-08 20:07:44 -0500

    added some error handling and form validation

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const { formatDate, formatTime } = require('./public/js/dates.js');
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
+const { formatDate, formatTime } = require('./public/js/dates.js'); 
 
 const Volume = require('./models/volume');
 
 const tanks = ["1A", "1B", "2A", "2B", "3A", "3B"];
 const zones = [1, 2, 3]
 
-mongoose.connect('mongodb://localhost:27017/fracTank')
+mongoose.connect('mongodb://localhost:27017/fracTank') 
     .then(() => {
         console.log("Mongo Connection Open")
     })
@@ -35,17 +37,17 @@ app.get('/', async (req, res) => {
 })
 
 //Read all volumes
-app.get('/volumes', async (req, res) => {   
+app.get('/volumes', catchAsync(async (req, res, next) => {   
     const volumes = await Volume.find({}); 
     res.render('tanks/index', { volumes });
-})
+}))
 
 //Read volumes by zone
-app.get('/volumes/:zones/view', async (req, res) => {
+app.get('/volumes/:zones/view', catchAsync(async (req, res) => {
     const {zones} = req.params;
     const volumes = await Volume.find({zone: zones})
     res.render('tanks/view', { volumes, formatDate, formatTime })
-})
+}))
  
 //Go to volume add page
 app.get('/volumes/:zone/new', (req, res) => { 
@@ -63,31 +65,43 @@ app.get('/volumes/:zone/new', (req, res) => {
 })
  
 //Post new data
-app.post('/volumes', async (req, res) => {
+app.post('/volumes', catchAsync(async (req, res) => {
     const newVolume = new Volume(req.body);
     await newVolume.save(); //Saved to db
     res.redirect(`/volumes/${newVolume.zone}/view`);   
-})
+}))
 
 //Edit an entry 
-app.get('/volumes/:id/edit', async (req, res) => {
+app.get('/volumes/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params
     const foundVolume = await Volume.findById(id);
     res.render('tanks/edit', { foundVolume, tanks, zones });
-})
+}))
 
-app.put('/volumes/:id', async (req, res) => {
+app.put('/volumes/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const volume = await Volume.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
     res.redirect(`/volumes/${volume.zone}/view`);
-})
+}))
 
-app.delete('/volumes/:id', async (req, res) => {
+app.delete('/volumes/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const deletedVolume = await Volume.findByIdAndDelete(id);
     res.redirect(`/volumes/${deletedVolume.zone}/view`);
+}))
+
+//for url we dont recognize - will respond if no matches - can use this in our below error handle
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+})
+
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    if(!err.message) err.message = "Oh No, Error"
+    res.status(statusCode).render('error', { err });//pass entire error to template
 })
 
 app.listen(3000, (req, res) => {
     console.log("Connected to 3000")
-}) 
+})  
